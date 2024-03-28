9245dfb127d8e2b22166de4b7a118acd72b646a6
megha395 <megha240397@gmail.com>
2024-03-29 01:52:16 +0530

    end point change

--- website.py
+++ website.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 #Routes
-@app.route('megha395.github.io/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
